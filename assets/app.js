a558a00a174f31220aa8e9324acd81f20e200725
Gustavo Canales <gustavo.canales@me.com>
2018-06-20 15:01:52 -0500

    Calendario automatico

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,14 @@
 var diaClase;
 var randomFormat = "YYYY/MM/DD"
-var horariosLunes = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m.", "5:00 p.m.", "6:00 p.m.", "7:00 p.m", "8:00 p.m."]
+var horariosLunes = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m.", "5:00 p.m.", "6:00 p.m.", "7:00 p.m.", "8:00 p.m."]
 var horariosMartes = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m.", "6:00 p.m.", "7:00 p.m", "8:00 p.m."]
-var horariosMiercoles = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m.", "5:00 p.m.", "6:00 p.m.", "7:00 p.m", "8:00 p.m."]
-var horariosJueves = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m.", "6:00 p.m.", "7:00 p.m", "8:00 p.m."]
+var horariosMiercoles = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m.", "5:00 p.m.", "6:00 p.m.", "7:00 p.m.", "8:00 p.m."]
+var horariosJueves = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m.", "6:00 p.m.", "7:00 p.m.", "8:00 p.m."]
 var horariosViernes = ["7:00 a.m.", "8:00 a.m.", "9:00 a.m.", "10:00 a.m."];
 var horariosSabado = ["9:00 a.m.", "10:00 a.m."]
+var diasSemana = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var codigoDia = ["A", "B", "C", "D", "E", "F"]
+var codigoHorario = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 // Initialize Firebase
 var config = {
@@ -32,18 +35,26 @@ $("#apartadoClase").on("click", function (event) {
     var horarioEscogidomodificado = horarioEscogido.replace(/\.|\s|:/g, "");
     console.log(horarioEscogidomodificado)
 
+    //codificar el horario y dia seleccionado
+    var indexhorario = horariosLunes.indexOf(horarioClase);
+    var renglonHorario = codigoHorario[indexhorario];
+    var indexdia = diasSemana.indexOf(diaDelaSemana);
+    var columnaHorario = codigoDia[indexdia];
+    var columnaReglon = columnaHorario + renglonHorario;
+
+
+
+
     if (diaDelaSemana === "Monday") {
         var disponibilidad = horariosLunes.indexOf(horarioClase)
         if (disponibilidad < 0) {
             $("#Respuesta").text("Te recordamos que no tenemos clase en ese horario por favor selecciona otro horario")
         }
         else {
-            dataRef.ref().child(horarioEscogidomodificado).on("value", function(snapshot) {
-                console.log("There are "+snapshot.numChildren()+" messages");
-              })
-            dataRef.ref().child(horarioEscogidomodificado).push({
+
+            dataRef.ref().child(columnaReglon).push({
                 nombre: nombreSocio,
-                
+
             })
         }
 
@@ -54,12 +65,26 @@ $("#apartadoClase").on("click", function (event) {
         if (disponibilidad < 0) {
             $("#Respuesta").text("Te recordamos que no tenemos clase en ese horario por favor selecciona otro horario")
         }
+        else {
+
+            dataRef.ref().child(columnaReglon).push({
+                nombre: nombreSocio,
+
+            })
+        }
     }
 
     if (diaDelaSemana === "Wednesday") {
         var disponibilidad = horariosMiercoles.indexOf(horarioClase)
         if (disponibilidad < 0) {
             $("#Respuesta").text("Te recordamos que no tenemos clase en ese horario por favor selecciona otro horario")
+        }
+        else {
+
+            dataRef.ref().child(columnaReglon).push({
+                nombre: nombreSocio,
+
+            })
         }
     }
 
@@ -68,12 +93,26 @@ $("#apartadoClase").on("click", function (event) {
         if (disponibilidad < 0) {
             $("#Respuesta").text("Te recordamos que no tenemos clase en ese horario por favor selecciona otro horario")
         }
+        else {
+
+            dataRef.ref().child(columnaReglon).push({
+                nombre: nombreSocio,
+
+            })
+        }
     }
 
     if (diaDelaSemana === "Friday") {
         var disponibilidad = horariosViernes.indexOf(horarioClase)
         if (disponibilidad < 0) {
             $("#Respuesta").text("Te recordamos que no tenemos clase en ese horario por favor selecciona otro horario")
+        }
+        else {
+
+            dataRef.ref().child(columnaReglon).push({
+                nombre: nombreSocio,
+
+            })
         }
     }
 
@@ -82,11 +121,13 @@ $("#apartadoClase").on("click", function (event) {
         if (disponibilidad < 0) {
             $("#Respuesta").text("Te recordamos que no tenemos clase en ese horario por favor selecciona otro horario")
         }
+        else {
+
+            dataRef.ref().child(columnaReglon).push({
+                nombre: nombreSocio,
+
+            })
+        }
     }
-
-
-
-
-
 });
 
